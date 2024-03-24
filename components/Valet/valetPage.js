19c3941e046d1e27f9303a3236881867f552f408
LazImane <164360620+LazImane@users.noreply.github.com>
2024-03-24 16:13:47 +0100

    Update valetPage.js

--- components/Valet/valetPage.js
+++ components/Valet/valetPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import Game from './board.js'
+import Game from './board.js';
+import Player from './cartesDeJoueur.js';
 
 const Valet = ({socket}) => {
   return (
     <div>
       <Game socket={socket}/>
+      <Player socket={socket}/>
     </div>
   );
 };
