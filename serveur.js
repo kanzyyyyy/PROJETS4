29454d31f12ec540a4159f0f4f24c7d0c9cd60a6
kanzyyyyy <141882650+kanzyyyyy@users.noreply.github.com>
2024-03-24 15:58:25 +0100

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -49,34 +49,41 @@ class DeckValet{
 
 }
 
- socket.on('start valet' , ()=> {
-         //faire disparaître le bouton 'Commencer' pour tous les joueurs
-        io.emit('commencer le jeu pout tout le monde');
+    socket.on('start valet' , ()=> {
         const deck = new DeckValet(); 
+        console.log(deck.deck.length); 
+
         console.log(deck); 
         utilisateurs.forEach((player) => {
             if(player.cartes.length == 0 ){
                 for(let i=0 ; i<51/nbJ ; i++ ){
                     let card = deck.distribuer(); 
+                    if(!card){
+                        break; 
+                    }
                     player.cartes.push(card); 
+                    //console.log("value after distribution loop :" +deck.deck.length); 
                 }
                 
 
             }}); 
-            console.log(deck.deck.length); 
-        while(deck.deck.length !=0){
+        console.log("value by the end of distribution loop " + deck.deck.length); 
+
+        while(deck.deck.length > 0 ){
                     let card = deck.distribuer(); 
+                    if(!card){
+                        break; 
+                    }
+                    if(card){
                     let randomindex= Math.floor(Math.random() * nbJ); 
                     utilisateurs[randomindex].cartes.push(card); 
-                }; 
+        }}; 
         utilisateurs.forEach(player => {
-                console.log(player.cartes); 
-                io.to(player.userId).emit('cards to player' , player.cartes); 
-
-            
+            console.log(player.cartes); 
+            console.log(player.cartes.length); 
+            io.to(player.userId).emit('cards to player' , player.cartes); 
         });
     }); 
-
  socket.on('chercherjoueurtourvalet', ()=> {
         data = globalTour ; 
         io.emit('tourdujoueur' , {userId : utilisateurs[data].userId , username : utilisateurs[data].username , userIdNEXT : utilisateurs[data+1].userId , usernameNEXT : utilisateurs[data+1].username}); 
