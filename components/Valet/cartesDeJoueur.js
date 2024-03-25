ae1750f1614448904e6a1b77f84b19c959b685b6
LazImane <164360620+LazImane@users.noreply.github.com>
2024-03-25 14:19:08 +0100

    Update cartesDeJoueur.js

--- components/Valet/cartesDeJoueur.js
+++ components/Valet/cartesDeJoueur.js
@@ -5,6 +5,8 @@ const Player = ({ socket }) => {
     const [nomJoueur, setNom] = useState('');
     const [showCartes, setShowCartes] = useState(false);
     const [cartes, setCartes] = useState([]);
+    const [click, setClickState] = useState(false);
+    const [nbClick, setNBClick] = useState(0);
 
     useEffect(() => {
         socket.on('cards to player', (player) => {
@@ -14,81 +16,50 @@ const Player = ({ socket }) => {
                 setShowCartes(true);
                 setNom(player.username);
                 setCartes(player.cartes);
+                if (player.position == 1) setClickState(true);
             }
         });
     }, [socket]);
 
-    console.log('les cartes de joueurs : ', nomJoueur, 'sont :', cartes);
-
     // Determine the number of columns based on screen width and card size
     const numColumns = Math.floor(window.innerWidth / 200); // Adjust 200 according to your card size
+
+    const HandleClick = (carte) => {
+        if (nbClick % 2 != 0) setClickState(false);
+        setNBClick(nbClick + 1);
+        console.log("selected card is : " + carte.number + " " + carte.symbol + " selected by " + nomJoueur);
+    };
 
     return (
         <div>
             {nomJoueur && <h2>{nomJoueur}</h2>}
-            <div className="card-container">
+            <div className="cartes-joueur">
                 {showCartes && cartes.filter(carte => carte).map((carte, index) => (
-                    <div key={index} className="card">
-                        <div className='front' data-symbol={carte.symbol}>
-                            <div className='number-top'>{carte.number}</div>
-                            <div className='symbol-top'>{carte.symbol}</div>
-                            <div className='number-bottom'>{carte.number}</div>
-                            <div className='symbol-bottom'>{carte.symbol}</div>
-                        </div>
+                    <div key={index} className="card" onClick={click ? () => HandleClick({ number: carte.number, score: carte.score }) : null}>
+                        {carte.symbol === "♥" || carte.symbol === "♦" ? (
+                            <div className='front1' data-symbol={carte.symbol}>
+                                <div className='number-top'>{carte.number}</div>
+                                <div className='symbol-top'>{carte.symbol}</div>
+                                <div className='number-bottom'>{carte.number}</div>
+                                <div className='symbol-bottom'>{carte.symbol}</div>
+                            </div>
+                        ) : (
+                            <div className='front2' data-symbol={carte.symbol}>
+                                <div className='number-top'>{carte.number}</div>
+                                <div className='symbol-top'>{carte.symbol}</div>
+                                <div className='number-bottom'>{carte.number}</div>
+                                <div className='symbol-bottom'>{carte.symbol}</div>
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
+
         </div>
     );
 };
 
 export default Player;
-
-/*import React, { useState, useEffect, useRef } from 'react';
-import './valet.css';
-
-const Player = ({ socket }) => {
-    const [nomJoueur, setNom] = useState('');
-    const [showcartes, setShowCartes] = useState(false);
-    const [cartes, setCartes] = useState([]);
-    useEffect(() => {
-        socket.on('cards to player', (players) => {
-            console.log('received cards to player event');
-            let player = players.find((player) => player.userId === socket.id);
-            if (player) {
-                setShowCartes(true);
-                setNom(player.username);
-                setCartes(player.cartes);
-            }
-
-        });
-    });
-    console.log('les cartes de joueurs : ', nomJoueur, 'sont :', cartes);
-    return (
-        <>
-            {nomJoueur && (<h2>{nomJoueur}</h2>)}
-            <table>
-                <tbody>
-                    <tr>
-                        {showcartes && cartes.filter(carte => carte).map((carte, index) => (
-                            <td key={index}>
-                                <div className='front' data-symbol={carte.symbol}>
-                                    <div className='number-top'>{carte.number}</div>
-                                    <div className='symbol-top'>{carte.symbol}</div>
-                                    <div className='number-bottom'>{carte.number}</div>
-                                    <div className='symbol-bottom'>{carte.symbol}</div>
-                                </div>
-                            </td>
-                        ))}
-                    </tr>
-                </tbody>
-
-            </table>
-        </>
-
-    )
-};
-export default Player;*/
 
 
 
